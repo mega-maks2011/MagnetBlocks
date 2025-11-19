3b38f1d23c31100023b5c1addde25ce8d8ef7dcb
mega-maks2011 <156597986+mega-maks2011@users.noreply.github.com>
2025-11-19 21:48:10 +0700

    Update ModBlockEntities.java

--- src/main/java/net/m998/magnetblocks/ModBlockEntities.java
+++ src/main/java/net/m998/magnetblocks/ModBlockEntities.java
@@ -9,10 +9,8 @@ public class ModBlockEntities {
     public static BlockEntityType<MagnetBlockEntity> MAGNET_BLOCK_ENTITY;
 
     public static void register() {
-        MAGNET_BLOCK_ENTITY = Registry.register(
-                Registries.BLOCK_ENTITY_TYPE,
+        MAGNET_BLOCK_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE,
                 new Identifier(MagnetBlocksMod.MOD_ID, "magnet_block_entity"),
-                BlockEntityType.Builder.create(MagnetBlockEntity::new, ModBlocks.MAGNET_BLOCK).build(null)
-        );
+                BlockEntityType.Builder.create(MagnetBlockEntity::new, ModBlocks.MAGNET_BLOCK).build(null));
     }
 }
