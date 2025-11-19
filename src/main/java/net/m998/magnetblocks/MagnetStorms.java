0a4f23dfeb0935cfb3aa90b8a24d85b27ec02328
mega-maks2011 <156597986+mega-maks2011@users.noreply.github.com>
2025-11-19 21:47:27 +0700

    Create MagnetStorms.java

---
+++ src/main/java/net/m998/magnetblocks/MagnetStorms.java
@@ -0,0 +1,19 @@
+package net.m998.magnetblocks;
+
+public class MagnetStorms {
+    public static final boolean ENABLE_MAGNETIC_STORMS = true;
+    public static final int MIN_STORM_INTERVAL = 60;
+    public static final int MAX_STORM_INTERVAL = 180;
+    public static final int MIN_STORM_DURATION = 2;
+    public static final int MAX_STORM_DURATION = 10;
+    public static final boolean STORM_POLARITY_INVERT = true;
+    public static final boolean STORM_RANDOM_POLARITY = true;
+    public static final boolean STORM_POWER_OFF = true;
+    public static final boolean STORM_POWER_BOOST = true;
+    public static final boolean STORM_POWER_REDUCE = true;
+    public static final boolean STORM_RANDOM_RADIUS = true;
+    public static final boolean AFFECT_HOSTILE_MOBS = true;
+    public static final boolean AFFECT_PASSIVE_ANIMALS = true;
+    public static final boolean AFFECT_VILLAGERS = true;
+    public static final boolean AFFECT_OTHER_ENTITIES = true;
+}
