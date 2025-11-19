1660427c53ee2ade304ac7d73b4a10496bac1ff4
mega-maks2011 <156597986+mega-maks2011@users.noreply.github.com>
2025-11-19 14:19:44 +0700

    Update ModItems.java

--- src/main/java/net/m998/magnetblocks/ModItems.java
+++ src/main/java/net/m998/magnetblocks/ModItems.java
@@ -10,25 +10,17 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item MAGNET_ITEM = new BlockItem(
-            ModBlocks.MAGNET_BLOCK,
-            new Item.Settings()
-    );
+    public static final Item MAGNET_ITEM = new BlockItem(ModBlocks.MAGNET_BLOCK, new Item.Settings());
 
     public static void register() {
-        Registry.register(Registries.ITEM,
-                new Identifier(MagnetBlocksMod.MOD_ID, "magnet"),
-                MAGNET_ITEM);
+        Registry.register(Registries.ITEM, new Identifier(MagnetBlocksMod.MOD_ID, "magnet"), MAGNET_ITEM);
 
-        // Create creative mode item group
         ItemGroup MAGNETS_GROUP = ItemGroup.create(ItemGroup.Row.TOP, 0)
                 .displayName(Text.translatable("itemGroup.magnetblocks.magnets"))
                 .icon(() -> new ItemStack(MAGNET_ITEM))
                 .entries((displayContext, entries) -> entries.add(MAGNET_ITEM))
                 .build();
 
-        Registry.register(Registries.ITEM_GROUP,
-                new Identifier(MagnetBlocksMod.MOD_ID, "magnets"),
-                MAGNETS_GROUP);
+        Registry.register(Registries.ITEM_GROUP, new Identifier(MagnetBlocksMod.MOD_ID, "magnets"), MAGNETS_GROUP);
     }
 }
